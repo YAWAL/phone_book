f62f92d6729aee07af9c4e1f9def6a3291c972a4
YAWAL <3dprinter.lviv@gmail.com>
2016-07-16 15:02:03 +0200

    in process

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,7 +1,5 @@
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by VYA on 02.07.2016.
@@ -31,11 +29,47 @@ public class PhoneBook {
 
     public void createContactTable(){
         try {
-            preparedStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS CONTACT (ID INT PRIMARY KEY AUTO_INCREMENT,NAME VARCHAR (20)),LASTNAME VARCHAR(40),MIDLNAME VARCHAR (40),PHJNENUMBER VARCHAR (20)");
+            preparedStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS CONTACTS (ID INT PRIMARY KEY AUTO_INCREMENT,CONTACTNAME VARCHAR (20),LASTNAME VARCHAR(40),MIDLNAME VARCHAR (40),PHONENUMBER VARCHAR (20))");
             preparedStatement.execute();
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    public void addContact(String contactName, String lastName, String midlName, String phoneNumber){
+        Contact contact = new Contact(contactName, lastName, midlName, phoneNumber);
+        try {
+            preparedStatement = connection.prepareStatement("INSERT INTO CONTACTS (contactName, lastName, midlName, phoneNumber) VALUES (?,?,?,?)");
+            preparedStatement.setString(1, contactName);
+            preparedStatement.setString(2,lastName);
+            preparedStatement.setString(3,midlName);
+            preparedStatement.setString(4,phoneNumber);
+            preparedStatement.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public ArrayList<Contact> findAllContacts(){
+        ArrayList<Contact> contacts = new ArrayList<>();
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = connection.prepareStatement("SELECT * FROM CONTACTS");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        try {
+            while (resultSet.next()){
+                Contact contact;
+                contact = new Contact(resultSet.getString("contactName"), resultSet.getString("lastName"), resultSet.getString("midlName"), resultSet.getString("phoneNumber"));
+                contact.setId(resultSet.getInt("id"));
+                contacts.add(contact);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return contacts;
     }
 
 
@@ -51,7 +85,7 @@ public class PhoneBook {
 //
 //        System.out.println("Enter name");
 //
-//        c.setName(scanner.next());
+//        c.setContactName(scanner.next());
 //
 //        System.out.println("Enter last name");
 //
@@ -70,7 +104,7 @@ public class PhoneBook {
 //
 //    public void showContact(){
 //
-//        System.out.println("Name" + "" + c.getName());
+//        System.out.println("Name" + "" + c.getContactName());
 //
 //    }
 
